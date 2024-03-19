9a38d663cc048b2be69b696c11bbc0f563e22bf2
Adilson-kina <134547014+Adilson-kina@users.noreply.github.com>
2024-03-19 14:40:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+let nome = window.prompt("Digite seu nome:");
+console.log("Seu nome é: " + nome);
+console.log("O tipo da variavel é:" + typeof(nome));
---
+++ soma.js
@@ -0,0 +1,5 @@
+let num1, num2, res;
+num1 = parseFloat(window.prompt("Digite o primeiro número"));
+num2 = parseFloat(window.prompt("Digite o segundo número"));
+res = num1 + num2;
+window.alert(`a soma é de ${res}`);  
