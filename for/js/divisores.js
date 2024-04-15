9b935ac8d78a0d4923a2f4c98c484b8f038580d0
Adilson-kina <134547014+Adilson-kina@users.noreply.github.com>
2024-04-15 16:06:38 -0300

    Added comments

--- for/js/divisores.js
+++ for/js/divisores.js
@@ -5,13 +5,21 @@
  *
  **/
 
+// array que futuramente terá os divisores de 120
 let divisores = [];
-num = 120;
+
+// o numero 120 em si, coloquei em variavel para ser possivel alterar com mais facilidate
+let num = 120;
+
+// loop para fazer a comparação de cada numero até 120
 for (let i = 0; i <= num; i++){
+  // checa se o numero é divisor de 120
   if (num % i == 0){
+    //se o numero for divisor de 120 adiciona ao array divisores
     divisores.push(i);
   }
 }
+//loop para imprimir cada valor do array divisores
 for (let i3 = 0; i3 < divisores.length; i3++){
   console.log(divisores[i3]);
 }
