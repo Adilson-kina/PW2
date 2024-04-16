a49651e89a4a39b42f035b4b7ea9f7c2b25c5a17
Adilson-kina <134547014+Adilson-kina@users.noreply.github.com>
2024-04-16 13:36:53 -0300

    Now with comments

--- for/js/onetohundred.js
+++ for/js/onetohundred.js
@@ -5,14 +5,19 @@
  *
  **/
 
-// Comparação para output de numeros pares e impares
+// loop para comparação para iterar para cada um dos valores
 for (let i = 0; i <= 100; i++) {
+  // comparação para saber se o valor é par
   if (i % 2 == 0) {
+    // output caso o valor seja par
     console.log(`Par: %d`, i)
   }
 } 
+// loop para comparação para iterar para cada um dos valores
 for (let i = 0; i <= 100; i++) {
+  // comparação para saber se o valor é impar
   if (i % 2 == 1) {
+    // output caso o valor seja impar
     console.log(`Impar: %d`, i)
   }
 }
